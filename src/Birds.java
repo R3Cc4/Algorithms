abad3cb21822acd35c28c7a7639e582460f762f4
vamsikrishna11 <vkp42@njit.edu>
2015-11-12 13:48:17 -0500

    for submission.

--- src/Birds.java
+++ src/Birds.java
@@ -2,27 +2,21 @@
  * Created by Vamsi on 11/9/2015.
  */
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 public class Birds {
-
     static final String inputFolderPath = "H:\\NJIT\\Fall 2015\\DataStructures & Algos CS610\\Problem sets\\Birds";
     static final String saveFilePath = inputFolderPath + "\\clean.ppm";
     static int fileSize = 0;
-    static int[][] pixels = new int[726408][18];
+    static int[][] pixels = new int[726408][19];
     static int fileNumber =0;
     static String[] configArray = new String[3];
-
     //This function Loads the Image Data from a filepath given in as parameter into the Pixels 2-D Array for Processing.
-    public static void LoadData(Path filePath){
+    public static void LoadData(String filePath) {
         BufferedReader reader = null;
-        File readFile = new File(String.valueOf(filePath));
+        File readFile = new File(inputFolderPath + "\\" + filePath);
         try {
             reader = new BufferedReader(new FileReader(readFile));
             String text = null;
-
             int i = 0;
             while((text=reader.readLine())!=null){
                 if(i>2){
@@ -47,7 +41,6 @@ public class Birds {
             }
         }
     }
-
     // This function Sorts the pixels in the 2D Array row wise. I.e, each row will be sorted making the 13th column as pivot.
     //hence all the elements to the left of 13th column are less and after it are more. I tried other values but 13th has the
     //most cleanest image!!
@@ -69,15 +62,12 @@ public class Birds {
     }
     public static void main(String args[])  {
         //This code will open the files in the specified path.
-        try {
-            Files.walk(Paths.get(inputFolderPath)).forEach(filePath -> {
-                if (Files.isRegularFile(filePath)) {
-                    //load file into the pixel 2D Array.
-                  LoadData(filePath);
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
+        File[] files = new File(inputFolderPath).listFiles();
+        //If this pathname does not denote a directory, then listFiles() returns null.
+        for (File file : files) {
+            if (file.isFile() && file.getName().contains("birds")) {
+                LoadData(file.getName());
+            }
         }
         int j = pixels.length;
         sortPixels();// sort all the rows in the pixel 2D array.
